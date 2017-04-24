9997fb6ccaff23a6bc309799e2c2b667f515d374
ricoo <ricoozf@gmail.com>
2017-04-24 19:05:16 +0800

    feat:add test

---
+++ tests/unit/CharacterTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Ricoa\CopyWritingCorrect\CopyWritingCorrectService;
+
+class CharacterTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+        $service=new CopyWritingCorrectService();
+
+
+        $this->assertEquals(
+            '这件蛋糕只卖 1000 元。',
+            $service->correct('这件蛋糕只卖１０００元。')
+        );
+
+        $this->assertEquals(
+            '乔布斯那句话是怎么说的？「Stay hungry, stay foolish.」',
+            $service->correct('乔布斯那句话是怎么说的？「Stay hungry, stay foolish.」')
+        );
+    }
+}
