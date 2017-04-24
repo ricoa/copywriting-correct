5c17e0c75d5b1a2dc9fd5f1e7740e4d080ec9013
ricoo <ricoozf@gmail.com>
2017-04-24 17:50:27 +0800

    feat:update interface

--- src/Correctors/Interfaces/Corrector.php
+++ src/Correctors/Interfaces/Corrector.php
@@ -2,7 +2,18 @@
 
 namespace Ricoa\CopyWritingCorrect\Correctors\Interfaces;
 
-interface Corrector{
+interface Corrector
+{
 
-	
+    /**
+     * @return mixed
+     */
+    public static function getInstance();
+
+    /**
+     * @param string $text
+     *
+     * @return mixed|void
+     */
+    public function handle($text);
 }
